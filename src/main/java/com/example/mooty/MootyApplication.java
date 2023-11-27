72147dcec0bcedf2bb364fd022998bf618528110
ramigou <dasul1021@gmail.com>
2023-11-27 23:04:43 +0900

    feat: initial spring boot and postgresql setting

---
+++ src/main/java/com/example/mooty/MootyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mooty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MootyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MootyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mooty/MootyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mooty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MootyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
